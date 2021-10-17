db14a0d73b8d2d1bd2cc7e2e5a8c16ab1887f2a6
ordonez93 <andresordonez2020@itp.edu.co>
2021-10-17 09:55:54 -0500

    entorno virtual

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template
+from werkzeug.exceptions import HTTPException
+app= Flask(__name__)
+
+@app.get('/')
+def inicio():
+    return render_template('index.html')
